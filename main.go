79fd4a2c5b93b54dd2f6ae3827c88c1a84a07274
Barossi <raul_barossi@hotmail.com>
2022-03-29 04:56:37 -0300

    Implementação da função pagar boleto com contrato entre as contas

--- main.go
+++ main.go
@@ -7,6 +7,25 @@ import (
 
 func main() {
 
+	fmt.Println("")
 	contaBruno := contas.CP{}
-	fmt.Println(contaBruno)
+	contaBruno.Depositar(150)
+	fmt.Println(contaBruno.ObterSaldo())
+	PagarBoleto(&contaBruno, 75)
+	fmt.Println(contaBruno.ObterSaldo())
+	fmt.Println("================")
+	contaBruna := contas.CC{}
+	contaBruna.Depositar(500)
+	fmt.Println(contaBruna.ObterSaldo())
+	PagarBoleto(&contaBruna, 450)
+	fmt.Println(contaBruna.ObterSaldo())
+	fmt.Println("")
+}
+
+func PagarBoleto(conta verificarConta, valorBoleto float64) {
+	conta.Sacar(valorBoleto)
+}
+
+type verificarConta interface {
+	Sacar(valor float64) string
 }
